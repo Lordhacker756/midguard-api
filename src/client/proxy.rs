9aeda70c866c6500658a6d5b2d0edb7bdaa6af8c
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2025-01-30 14:19:27 +0530

    FEAT: add swap history fetching functionality for the past 2 months

--- src/client/proxy.rs
+++ src/client/proxy.rs
@@ -2,6 +2,7 @@ use chrono::Utc;
 
 use crate::model::responses::{
     DepthPriceHistoryResponse, EarningHistoryResponse, EarningInterval, Interval,
+    SwapHistoryResponse, SwapInterval,
 };
 
 pub async fn get_prev_2_months_price_history() -> Result<Vec<Interval>, reqwest::Error> {
@@ -40,7 +41,7 @@ pub async fn get_prev_2_months_price_history() -> Result<Vec<Interval>, reqwest:
     Ok(final_data)
 }
 
-pub async fn get_prev_2_monts_earning_history() -> Result<Vec<EarningInterval>, reqwest::Error> {
+pub async fn get_prev_2_months_earning_history() -> Result<Vec<EarningInterval>, reqwest::Error> {
     let now = Utc::now();
     let timestamp = now.timestamp();
     let mut from = 1730419200;
@@ -63,6 +64,44 @@ pub async fn get_prev_2_monts_earning_history() -> Result<Vec<EarningInterval>, 
         let res = reqwest::get(&url)
             .await?
             .json::<EarningHistoryResponse>()
+            .await?;
+
+        final_data.extend(res.intervals);
+        let idx = final_data.len() - 1;
+        from = final_data[idx].endTime.parse().expect("Not a valid string")
+    }
+
+    println!(
+        "DONE last timestamp @{} and total entries are {}",
+        final_data[final_data.len() - 1].endTime,
+        final_data.len()
+    );
+    Ok(final_data)
+}
+
+pub async fn get_prev_2_months_swap_history() -> Result<Vec<SwapInterval>, reqwest::Error> {
+    let now = Utc::now();
+    let timestamp = now.timestamp();
+    let mut from = 1730419200;
+
+    let mut final_data: Vec<SwapInterval> = Vec::new();
+
+    while from < timestamp {
+        let url = format!(
+            "https://midgard.ninerealms.com/v2/history/swaps?interval=hour&from={}&count=400",
+            from
+        );
+
+        println!("GET:: {}", url);
+        println!(
+            "Current Timestamp:: {} | Limit Timestamp:: {} | Is it less:: {}",
+            from,
+            timestamp,
+            from < timestamp
+        );
+        let res = reqwest::get(&url)
+            .await?
+            .json::<SwapHistoryResponse>()
             .await?;
 
         final_data.extend(res.intervals);
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod client;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    let res = client::proxy::get_prev_2_monts_earning_history()
+    let res = client::proxy::get_prev_2_months_swap_history()
         .await
         .unwrap();
 
